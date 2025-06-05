fed7840eaf7658e1f80afb9304194bb23f3d1c20
Mahesh9093 <90627293+Mahesh9093@users.noreply.github.com>
2025-06-05 23:56:03 +0530

    Create user-creation.sh
    Simply user creation

---
+++ user-creation.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#user creation shell script 
+# Input from user to check the user avalible or not
+read -p "Enter username: " username
+read -p "Enter password: " password
+echo "Password: $password"
+
+# Check if the user already exists
+if id -u "$username" &> /dev/null; then
+  echo "User '$username' already exists."
+  exit 1
+fi
+
+# add user 
+sudo useradd -m -s /bin/bash "$username"
+echo "$username:$password" | sudo chpasswd
+
+#show user is created 
+echo "User '$username' created successfully."
